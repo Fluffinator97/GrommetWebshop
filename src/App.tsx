d58ea88e9536b7857a0cb800ec5842039cf4a03e
mamta srinivas <mamtasrinivassp@gmail.com>
2020-03-21 22:10:28 +0100

    Basic routing to checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ProductList from './productList'
-import {ShopCart} from './ShopCart'
-import { CartProvider } from './context/cartContext';
-
-
+import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary';
+import Checkout from './Checkout';
+import HomePage from './HomePage';
 
 function App() {
   return (
+
     <div className="App">
-      <CartProvider>
-        <div>
-          <ShopCart />
-          <ProductList />
-        </div>
-      </CartProvider>
+
+
+      <Switch>
+
+        <Route path="/Checkout">
+          <ErrorBoundary>
+            <Checkout />
+          </ErrorBoundary>
+        </Route>
+        <Route path="/">
+          <ErrorBoundary>
+            <HomePage />
+          </ErrorBoundary>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/ShopCart.tsx
+++ src/ShopCart.tsx
@@ -1,23 +1,35 @@
-import React, { CSSProperties, useContext } from 'react'
-import { Shop } from 'grommet-icons'
-import { grommet, Box, Button, Grommet } from "grommet";
+import React, { CSSProperties, useContext, useState } from 'react'
+import { Shop, Link } from 'grommet-icons'
+import { Button } from "grommet";
 import { CartContext } from "./context/cartContext";
+import { Redirect } from 'react-router-dom'
 
 interface Props {
 
 }
 
+
+
 export const ShopCart = (props: Props) => {
+    const [checkOut, setCheckOut] = useState(false)
     const [cart, setCart] = useContext(CartContext)
     const totalPrice = cart.reduce((acc: number, curr: { price: number; }) => acc + curr.price, 0)
+
+    const renderRedirect = () => {
+        if (checkOut) {
+            return <Redirect to='/Checkout' />
+        }
+    }
+
+
     return (
         <>
-
+            {renderRedirect()}
             <span style={badgeStyle}>{cart.length}</span>
 
             <Button
                 icon={<Shop />}
-                onClick={() => { }}
+                onClick={() => setCheckOut(true)}
             />
 
             {/* {cart.map((item: { name: React.ReactNode; price: React.ReactNode; }) => (<h6>{item.name} - {item.price}</h6>))} */}
