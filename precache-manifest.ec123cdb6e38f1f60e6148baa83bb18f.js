90fca9ff65011a874f60335f152c11eb46b2bdae
mamta srinivas <mamtasrinivassp@gmail.com>
2020-04-03 07:54:42 +0200

    Updates

---
+++ precache-manifest.ec123cdb6e38f1f60e6148baa83bb18f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc8c019bc575db5fd72327bf4157a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3667d65d706c85ff13f",
+    "url": "/static/css/main.9fcf6ba8.chunk.css"
+  },
+  {
+    "revision": "289e780749a46c93daeb",
+    "url": "/static/js/2.c336e356.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.c336e356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3667d65d706c85ff13f",
+    "url": "/static/js/main.7bcbb385.chunk.js"
+  },
+  {
+    "revision": "7589f06785c8fca42006",
+    "url": "/static/js/runtime-main.2d03760f.js"
+  },
+  {
+    "revision": "3e4746d96e10fd4e3735adf3fe8b65f2",
+    "url": "/static/media/dhl.3e4746d9.png"
+  },
+  {
+    "revision": "285dd376d0f4586a5cc0ccc102b8d002",
+    "url": "/static/media/payment.285dd376.gif"
+  },
+  {
+    "revision": "36732f8eae41889332190c502cefd042",
+    "url": "/static/media/shopping_cart.36732f8e.svg"
+  },
+  {
+    "revision": "881433d0fcab799e3389256cab669c20",
+    "url": "/static/media/successful_purchase.881433d0.svg"
+  }
+]);
